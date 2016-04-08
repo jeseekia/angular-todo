791ada822177979d476d6b90ed37a98ba7e3b0d6
Jeseekia Vaughn <jeseekia@gmail.com>
2016-04-07 23:55:26 -0400

    Adding an array of todos to the mainCtrl scope.

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,16 @@
 angular.module("todoListApp",[])
 .controller('mainCtrl',function($scope){
-  $scope.helloWorld = function(){console.log("helloWorld controller function in the main controller")};
+  $scope.helloWorld = function() {
+    console.log("helloWorld controller function in the main controller");
+  };
+
+
+$scope.todos = [
+  {"name": "finish weekly blogs"},
+  {"name": "choose l.u.y.l. quest categories"},
+  {"name": "grade assessments"},
+  {"name": "hackathon workshop slides"},
+  {"name": "schedule GDI tweets"},
+  {"name": "make a timeline for moving"},
+]
 });
